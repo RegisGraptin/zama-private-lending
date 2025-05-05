0943807a0d4d8cc66bf31bee3c3c89b7e6730c32
rere <regis-graptin@sfr.fr>
2025-05-05 14:04:10 +0200

    deposit/withdraw - try leverage wrap/unwrap

--- contracts/contracts/ConfidentialLendingLayer.sol
+++ contracts/contracts/ConfidentialLendingLayer.sol
@@ -8,6 +8,9 @@ import "fhevm-contracts/contracts/token/ERC20/ConfidentialERC20Wrapped.sol";
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 import { IPool } from "@aave-v3-core/contracts/protocol/pool/Pool.sol";
 import { IPoolAddressesProvider } from "@aave-v3-core/contracts/protocol/configuration/PoolAddressesProvider.sol";
 
@@ -15,8 +18,24 @@ import { IPoolAddressesProvider } from "@aave-v3-core/contracts/protocol/configu
 /// @dev It supports typical ERC20 functionality such as transferring tokens, minting, and setting allowances,
 /// @dev but uses encrypted data types.
 contract ConfidentialLendingLayer is ConfidentialERC20Wrapped {
+    using SafeERC20 for IERC20Metadata;
+
+    address asset;
+    euint256 nextRoundAction;
+
+    // Track user balance
+
+    //  balances --> available
+    // lending
+    mapping(address account => euint256 balance) internal _availableBalance; // Waiting balance
+    mapping(address account => euint256 balance) internal _lendingBalance; // Balance in lending
+
     /// @notice Address of the AAVE Pool Address Provider allowing us to fetch the pool address
     address public immutable POOL_ADDRESSES_PROVIDER_ADDRESS;
+
+    function _aavePoolAddress() internal view returns (address) {
+        return IPoolAddressesProvider(POOL_ADDRESSES_PROVIDER_ADDRESS).getPool();
+    }
 
     constructor(
         address _aaveProviderAddress,
@@ -24,22 +43,87 @@ contract ConfidentialLendingLayer is ConfidentialERC20Wrapped {
         uint256 maxDecryptionDelay_
     ) ConfidentialERC20Wrapped(erc20_, maxDecryptionDelay_) {
         POOL_ADDRESSES_PROVIDER_ADDRESS = _aaveProviderAddress;
+        asset = erc20_;
+
+        // Negative number not managed
+        nextRoundAction = TFHE.asEuint256(1e18);
+        TFHE.allowThis(nextRoundAction);
     }
 
-    function _aavePoolAddress() internal view returns (address) {
-        return IPoolAddressesProvider(POOL_ADDRESSES_PROVIDER_ADDRESS).getPool();
+    function deposit(uint256 amount) external {
+        ERC20_TOKEN.safeTransferFrom(msg.sender, address(this), amount);
+
+        // Increase user available balance
+        euint256 newAvailableBalance = _availableBalance[msg.sender] + TFHE.asEuint256(amount);
+        _availableBalance[msg.sender] = newAvailableBalance;
+
+        // Authorize access
+        TFHE.allowThis(newAvailableBalance);
+        TFHE.allow(newAvailableBalance, msg.sender);
     }
 
-    function supply(address asset, uint256 amount) external {
-        IERC20(asset).transferFrom(msg.sender, address(this), amount);
+    function withdraw(uint256 amount) external {
+        // Verify the user has enough funds
+        euint256 eAmount = TFHE.asEuint256(amount);
+        ebool canWithdraw = TFHE.le(eAmount, _availableBalance[msg.sender]);
+        euint256 transferValue = TFHE.select(canWithdraw, eAmount, TFHE.asEuint256(0));
+
+        // Call the gateway to decrypt and transfer the value
+
+        uint256[] memory cts = new uint256[](1);
+        cts[0] = Gateway.toUint256(eAmount);
+        uint256 requestId = Gateway.requestDecryption(
+            cts,
+            this.gatewayCallbackDecryptBid.selector,
+            0,
+            block.timestamp + 100,
+            false
+        );
+    }
+
+    function lendToAave(einput eRequestedAmount, bytes calldata inputProof) external {
+        euint256 eAmount = TFHE.asEuint256(eRequestedAmount, inputProof);
+
+        // Verify enough funds
+        ebool canTransfer = TFHE.le(amount, _balances[msg.sender]);
+
+        euint256 newBalanceAccount = TFHE.add(_lending[msg.sender], eAmount);
+        _lending[msg.sender] = newBalanceAccount;
+
+        TFHE.allowThis(newBalanceAccount);
+        TFHE.allow(newBalanceAccount, msg.sender);
+    }
+
+    function withdrawFromAave(einput eRequestedAmount, bytes calldata inputProof) external {
+        euint256 eAmount = TFHE.asEuint256(eRequestedAmount, inputProof);
+
+        euint256 newBalanceAccount = TFHE.sub(_lending[msg.sender], eAmount);
+        _lending[msg.sender] = newBalanceAccount;
+
+        TFHE.allowThis(newBalanceAccount);
+        TFHE.allow(newBalanceAccount, msg.sender);
+    }
+
+    function executeRound(uint256 requestId, uint256 lendingAmout) external onlyGateway {
+        if (lendingAmout > 1e18) {
+            // Lending action
+            _aaveSupply(lendingAmout - 1e18);
+        } else {
+            _aaveWithdraw(1e18 - lendingAmout);
+        }
+    }
+
+    //// Gateway callback
+
+    function _aaveSupply(uint256 amount) internal {
         IERC20(asset).approve(_aavePoolAddress(), amount);
         IPool(_aavePoolAddress()).supply(asset, amount, address(this), 0);
 
         // TODO: Add event
     }
 
-    function withdraw(address asset, uint256 amount) external {
-        uint256 amountWitdraw = IPool(_aavePoolAddress()).withdraw(asset, amount, msg.sender);
+    function _aaveWithdraw(uint256 amount) internal {
+        IPool(_aavePoolAddress()).withdraw(asset, amount, msg.sender);
 
         // TODO: Add event
     }
